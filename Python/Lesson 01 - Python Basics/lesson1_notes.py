9fbdc104ad086489ae7f779a15aa90006234f321
david <dnharrington@proton.me>
2026-07-10 17:41:31 -0400

    EOD_day2

---
+++ Python/Lesson 01 - Python Basics/lesson1_notes.py
@@ -0,0 +1,10 @@
+diam = int(input("What is the diamter of your pizza?"))
+
+print("The area of your pizza is", 3.14 * (diam/2) **2)
+
+cost = int(input("What does this pizza cost?"))
+
+print(f"The cost per area of your pizza is {cost/(3.14 * (diam/2) ** 2):.3f}")
+
+
+
