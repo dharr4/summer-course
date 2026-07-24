dedc75bc1649a6184d44ad986449ee9c134fe61e
dharr4 <dnharrington@proton.me>
2026-07-24 12:07:47 -0400

    afternoonpush

--- Python/Lesson 06 - Error Handling/Lesson6Notes.py
+++ Python/Lesson 06 - Error Handling/Lesson6Notes.py
@@ -1,7 +1,22 @@
-#morning exercise
+# #morning exercise
 
-import random
-with open('numbers.txt', 'w') as file:
-    for line in range(100):
-        random_numbers = random.randint(1, 1000)
-        file.write(str(random_numbers) + '\n')
+# import random
+# with open('numbers.txt', 'w') as file:
+#     for line in range(100):
+#         random_numbers = random.randint(1, 1000)
+#         file.write(str(random_numbers) + '\n')
+
+#     new_list = []
+#     for line in lines:
+#         line = line.strip()
+
+#     lines_stripped = [int(line.strip()) for line in lines]
+#     #print(lines_stripped)
+
+# #click in margins for red bubble to add code breaks
+
+#python has built in exceptions that raise when errors happen to help programmers. type error, syntax errr, indentation, divide by 0.
+#help learn to debug.
+
+for row in range(3):
+    print('1 2 3')
